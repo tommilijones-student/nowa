762382f0859a2f3a7cbfdf6a0068d61db3b8824a
Tom <11tommilijones11@gmail.com>
2025-06-15 13:38:57 +0200

    poczatek

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, GitHub!\n");
+
+    // Suma liczb od 1 do 5
+    int sum = 0;
+    for (int i = 1; i <= 5; i++) {
+        sum += i;
+    }
+
+    printf("Suma liczb od 1 do 5 to: %d\n", sum);
+
+    return 0;
+}
